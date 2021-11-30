c2f829deff18332b8ecd0f6cacc83cbbc3ebf62a
Liv Huang <livhuang95@gmail.com>
2021-11-30 21:27:32 +0800

    add logout function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,24 +12,23 @@ class UserController extends Controller
     }
 
     public function doLogin(Request $request) {
-        echo 'test1 <br>';
         $credentials = $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
-        echo 'test2 <br>';
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
             return redirect('/');
         }
-        echo 'test3 <br>';
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
         ]);
     }
 
     public function logout(Request $request) {
-        // $rooms = Room::all();
-        // return view('index', ['rooms' => $rooms]);
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
     }
 }
