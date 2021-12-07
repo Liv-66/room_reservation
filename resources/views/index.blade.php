43c218878e8c280960a95037eac7b0ee5a716999
Liv Huang <livhuang95@gmail.com>
2021-12-07 20:45:03 +0800

    add public path

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,7 @@
     @foreach($rooms as $room)
     <div class="col-md-4">
         <div class="card mb-4 shadow-sm">
-            <img class="card-img-top" src="storage/{{ $room->photo }}" alt="Card image cap" width="286px" height="180px">
+            <img class="card-img-top" src="img/{{ $room->photo }}" alt="Card image cap" width="286px" height="180px">
             <div class="card-body">
                 <p class="card-text">
                     <a href="/api/v1/rooms/{{ $room->id }}">
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homestay</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel='shortcut icon' type='image/png' href={{ url('storage/pumkin.png') }}>
-    <link rel="stylesheet" href={{ url('storage/style.css') }}>
+    <link rel='shortcut icon' type='image/png' href={{ asset('img/pumkin.png') }}>
+    <link rel="stylesheet" href={{ asset('css/app.css') }}>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
